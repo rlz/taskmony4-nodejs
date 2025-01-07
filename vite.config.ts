8e0f2f1c77c7699e56f2f93d817ff57c502b18c6
Dmitry Maslennikov <maslennikovdm@gmail.com>
2025-01-07 07:06:25 +0300

    Use vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     root: './client-web',
-    plugins: [react()]
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Taskmony',
+                description: 'Personal task tracker',
+                icons: [
+                    {
+                        src: '/favicon.svg',
+                        sizes: 'any'
+                    }
+                ],
+                background_color: '#181818',
+                theme_color: '#9CC8F5',
+                scope: '/'
+            },
+
+            devOptions: {
+                enabled: true
+            }
+        })
+    ]
 })

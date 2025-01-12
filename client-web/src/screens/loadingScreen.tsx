7f9c3e38eca09e923b202b2b290ebbff161f1c13
Dmitry Maslennikov <maslennikovdm@gmail.com>
2025-01-12 17:44:22 +0300

    Better loading screen

--- client-web/src/screens/loadingScreen.tsx
+++ client-web/src/screens/loadingScreen.tsx
@@ -4,10 +4,15 @@ import React, { JSX } from 'react'
 export function LoadingScreen(): JSX.Element {
     return (
         <Stack alignItems={'center'}>
-            <Box pt={'20vh'} width={'40vw'}>
-                <img src={'/favicon.svg'} />
+            <Box pt={'20vh'} width={'min(30vw, 20vh)'}>
+                <img src={'/icon.svg'} />
             </Box>
-            <Box textAlign={'center'} fontSize={'15vw'}>{'Loading...'}</Box>
+            <Box
+                textAlign={'center'}
+                fontSize={'min(12vw, 8vh)'}
+            >
+                {'Loading...'}
+            </Box>
         </Stack>
     )
 }
